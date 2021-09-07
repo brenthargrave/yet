e0fe5348f80068e835d384a48cfe1ae7caf4c7ec
Brent Hargrave <brent@brent.is>
2021-09-07 15:18:43 -0500

    . recall now why host isn't set yet

--- web/config/prod.exs
+++ web/config/prod.exs
@@ -3,7 +3,6 @@ use Mix.Config
 config :app, AppWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  # TODO url: [host: "example.com", port: 80],
   url: [scheme: "https", port: 443]
 
 config :logger, level: :info
