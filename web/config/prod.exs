7f19db3cf32277513bfca6c876d6f8a8affba6ba
Brent Hargrave <brent@brent.is>
2021-09-07 19:03:55 -0500

    . restore prod configs

--- web/config/prod.exs
+++ web/config/prod.exs
@@ -3,14 +3,8 @@ use Mix.Config
 config :app, AppWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  url: [scheme: "https", port: 443],
-  https: [
-    port: System.get_env("PORT_SSL"),
-    cipher_suite: :strong,
-    keyfile: "priv/cert/localhost-key.pem",
-    certfile: "priv/cert/localhost-cert.pem"
-  ]
+  url: [scheme: "https", port: 443]
 
 config :logger, level: :info
 
-# config :app, App.Repo, ssl: true
+config :app, App.Repo, ssl: true
