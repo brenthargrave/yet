65a92bba8e73f26f92165e3dd358ee77599bf50e
Brent Hargrave <brent@brent.is>
2021-09-08 15:07:53 -0500

    add .ico as binary

--- web/lib/app_web/endpoint.ex
+++ web/lib/app_web/endpoint.ex
@@ -22,9 +22,10 @@ defmodule AppWeb.Endpoint do
   # You should set gzip to true if you are running phx.digest
   # when deploying your static files in production.
   plug Plug.Static,
-    # TODO only: ~w(css fonts images js favicon.ico robots.txt _snowpack)
     at: "/",
     from: :app,
+    # TODO: restore w/ snowpack compat
+    # only_matching: ~w(css fonts images js favicon.ico robots.txt _snowpack)
     gzip: true
 
   # Code reloading can be explicitly enabled under the
