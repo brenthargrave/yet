e95bf684a9f545d7e41ce39eaffc5766a68da143
Brent Hargrave <brent@brent.is>
2021-09-16 12:58:33 -0500

    . closer

---
+++ web/ui/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "airbnb",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["prettier", "react", "react-hooks", "@typescript-eslint"],
+  rules: {},
+}
