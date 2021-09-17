e751d4c92630f116bf8b4b1c0c4a852e2b4751a4
Brent Hargrave <brent@brent.is>
2021-09-17 15:39:14 -0500

    . app view (splash

--- web/ui/static/src/components/app/view.ts
+++ web/ui/static/src/components/app/view.ts
@@ -1,3 +1,24 @@
+import { h } from "@cycle/react"
 import { h1 } from "@cycle/react-dom"
+import { Stack, Button } from "@mui/material"
 
-export const view = () => h1("Hello, world!")
+const logo = h1(`TBD`)
+const button = h(
+  Button,
+  {
+    variant: "outlined",
+  },
+  `Sign up`
+)
+
+export const view = () =>
+  h(
+    Stack,
+    {
+      direction: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      spacing: 2,
+    },
+    [logo, button]
+  )
