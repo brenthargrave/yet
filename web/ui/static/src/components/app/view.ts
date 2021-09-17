1222a6e1092b1c81f76c14178679d34fe2695a76
Brent Hargrave <brent@brent.is>
2021-09-17 16:27:58 -0500

    . drop mui

--- web/ui/static/src/components/app/view.ts
+++ web/ui/static/src/components/app/view.ts
@@ -1,24 +1,24 @@
 import { h } from "@cycle/react"
 import { h1 } from "@cycle/react-dom"
-import { Stack, Button } from "@mui/material"
+// import { Stack, Button } from "@mui/material"
 
 const logo = h1(`TBD`)
-const button = h(
-  Button,
-  {
-    variant: "outlined",
-  },
-  `Sign up`
-)
+// const button = h(
+//   Button,
+//   {
+//     variant: "outlined",
+//   },
+//   `Sign up`
+// )
 
-export const view = () =>
-  h(
-    Stack,
-    {
-      direction: "column",
-      justifyContent: "center",
-      alignItems: "center",
-      spacing: 2,
-    },
-    [logo, button]
-  )
+export const view = () => logo
+// h(
+//   Stack,
+//   {
+//     direction: "column",
+//     justifyContent: "center",
+//     alignItems: "center",
+//     spacing: 2,
+//   },
+//   [logo, button]
+// )
