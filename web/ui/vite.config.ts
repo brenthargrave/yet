9d96e45b84bf4b16a1a6c096d1e4f67bb5414de0
Brent Hargrave <brent@brent.is>
2021-09-18 19:13:18 -0500

    amazing: yarn vite Just Works

--- web/ui/vite.config.ts
+++ web/ui/vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from "vite"
 import reactRefresh from "@vitejs/plugin-react-refresh"
 
+import fs from "fs"
+import path from "path"
+
+let key
+let cert
+try {
+  key = fs.readFileSync(path.resolve("../priv/cert/localhost-key.pem"))
+  cert = fs.readFileSync(path.resolve("../priv/cert/localhost-cert.pem"))
+} catch (error) {
+  console.error(error)
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
+  server: {
+    https: { key, cert },
+  },
 })
