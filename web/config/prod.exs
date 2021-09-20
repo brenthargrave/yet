09f513f815981273d02a719cbc51e4d0bd4517ab
Brent Hargrave <brent@brent.is>
2021-09-20 13:18:56 -0500

    . try: commenting out static manifest

--- web/config/prod.exs
+++ web/config/prod.exs
@@ -1,7 +1,8 @@
 use Mix.Config
 
 config :app, AppWeb.Endpoint,
-  cache_static_manifest: "priv/static/cache_manifest.json",
+  # TODO: restore
+  # cache_static_manifest: "priv/static/cache_manifest.json",
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   url: [scheme: "https", port: 443]
 
