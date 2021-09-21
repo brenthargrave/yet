67124a68d19ec970fb187ef473b456f88d19e7c4
Brent Hargrave <brent@brent.is>
2021-09-21 15:22:15 -0500

    . bleh: can't use hooks

--- web/ui/src/components/Auth/PhoneSubmit/index.ts
+++ web/ui/src/components/Auth/PhoneSubmit/index.ts
@@ -1,5 +1,6 @@
 import { h } from "@cycle/react"
 import { useState } from "react"
+import { phone as validatePhone } from "phone"
 
 import { view } from "./view"
 
@@ -8,6 +9,8 @@ export const PhoneSubmit = () => {
   const [isDisabled, setDisabled] = useState<boolean>(true)
   const onChangePhone: React.ChangeEventHandler<HTMLInputElement> = (event) => {
     setPhone(event.currentTarget.value)
+    const { isValid } = validatePhone(phone, { country: "USA" })
+    setDisabled(!isDisabled)
   }
   return h(view, { phone, onChangePhone, isDisabled })
 }
