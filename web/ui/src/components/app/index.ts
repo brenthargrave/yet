3452dd0e7d4c49b84d84df9279a95161b8e07348
Brent Hargrave <brent@brent.is>
2021-09-21 11:08:18 -0500

    ...

--- web/ui/src/components/app/index.ts
+++ web/ui/src/components/app/index.ts
@@ -1,10 +1,6 @@
 import { h } from "@cycle/react"
-
 import { ChakraProvider } from "@chakra-ui/react"
-import { Landing } from "../Landing"
 
-const { VITE_API_ENV } = import.meta.env
-// eslint-disable-next-line no-console
-console.log(`API_ENV: ${VITE_API_ENV}`)
+import { Landing } from "../Landing"
 
 export const App = () => h(ChakraProvider, {}, [h(Landing)])
