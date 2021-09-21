bc69719ffb3100f6861a3b162b066838fc3f3678
Brent Hargrave <brent@brent.is>
2021-09-21 11:56:29 -0500

    . disclaimer

--- web/ui/src/components/Landing/view.ts
+++ web/ui/src/components/Landing/view.ts
@@ -1,7 +1,14 @@
 import { h } from "@cycle/react"
-import { Center, Stack, Heading } from "~/system"
+import { Center, Stack, Heading, Text } from "~/system"
 
 export const view = () =>
   h(Center, { width: "100vw", height: "100vh" }, [
-    h(Stack, { direction: "column" }, [h(Heading, {}, "T.B.D.")]),
+    h(Stack, { direction: "column", align: "center" }, [
+      h(Heading, {}, "T.B.D."),
+      h(
+        Text,
+        {},
+        `By tapping Create Account or Sign in you agree to never sue me.`
+      ),
+    ]),
   ])
--- web/ui/src/system/index.ts
+++ web/ui/src/system/index.ts
@@ -6,8 +6,11 @@ import {
   StackProps,
   Heading as _Heading,
   HeadingProps,
+  Text as _Text,
+  TextProps,
 } from "@chakra-ui/react"
 
 export const Center = (props: CenterProps) => h(_Center, props)
 export const Heading = (props: HeadingProps) => h(_Heading, props)
 export const Stack = (props: StackProps) => h(_Stack, props)
+export const Text = (props: TextProps) => h(_Text, props)
