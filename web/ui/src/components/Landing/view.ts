f37fd6d2a58ccefdd21a677b00e5e724b783f1f5
Brent Hargrave <brent@brent.is>
2021-09-21 11:40:44 -0500

    . ... stick to rule of 2

--- web/ui/src/components/Landing/view.ts
+++ web/ui/src/components/Landing/view.ts
@@ -1,8 +1,7 @@
 import { h } from "@cycle/react"
 import { Heading, Stack } from "@chakra-ui/react"
 
-// TODO: wrap compnents w/ props
 export const view = () =>
   h(Stack, { direction: "column", width: "100vw", height: "100vh" }, [
-    h(Heading, {}, "TBD"),
+    h(Heading, {}, "T.B.D."),
   ])
