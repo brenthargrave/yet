6e00f09c1b4b17d76a9f45d581cb7ccd79b621d5
Brent Hargrave <brent@brent.is>
2021-09-21 12:12:15 -0500

    . i18n

--- web/ui/src/components/Landing/view.ts
+++ web/ui/src/components/Landing/view.ts
@@ -1,10 +1,12 @@
 import { h } from "@cycle/react"
+
 import { Center, Stack, Heading, Text } from "~/system"
+import { t } from "~/i18n"
 
 export const view = () =>
   h(Center, { width: "100vw", height: "100vh" }, [
     h(Stack, { direction: "column", align: "center" }, [
-      h(Heading, {}, "T.B.D."),
+      h(Heading, {}, t("brand-name")),
       h(
         Text,
         {},
---
+++ web/ui/src/i18n/index.ts
@@ -0,0 +1,6 @@
+export const t = (key: string): string => {
+  const messages: Record<string, string> = {
+    "brand-name": "T.B.D.",
+  }
+  return messages[key]
+}
