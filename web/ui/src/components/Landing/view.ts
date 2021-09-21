c20cc186d96c4e00823f1021ab80a4f5a0a00463
Brent Hargrave <brent@brent.is>
2021-09-21 12:16:38 -0500

    . lame styles

--- web/ui/src/components/Landing/view.ts
+++ web/ui/src/components/Landing/view.ts
@@ -5,11 +5,11 @@ import { t } from "~/i18n"
 
 export const view = () =>
   h(Center, { width: "100vw", height: "100vh" }, [
-    h(Stack, { direction: "column", align: "center" }, [
+    h(Stack, { direction: "column", align: "center", margin: "4" }, [
       h(Heading, {}, t("brand-name")),
       h(
         Text,
-        {},
+        { fontSize: "x-small" },
         `By tapping Create Account or Sign in you agree to never sue me.`
       ),
     ]),
