1e09ec544af22b78dde494940996e119b8acefd0
Brent Hargrave <brent@brent.is>
2021-09-21 11:48:00 -0500

    . extract system

--- web/ui/src/components/Landing/view.ts
+++ web/ui/src/components/Landing/view.ts
@@ -1,5 +1,5 @@
 import { h } from "@cycle/react"
-import { Heading, Stack } from "@chakra-ui/react"
+import { Stack, Heading } from "~/system"
 
 export const view = () =>
   h(Stack, { direction: "column", width: "100vw", height: "100vh" }, [
---
+++ web/ui/src/system/index.ts
@@ -0,0 +1,10 @@
+import { h } from "@cycle/react"
+import {
+  Stack as _Stack,
+  StackProps,
+  Heading as _Heading,
+  HeadingProps,
+} from "@chakra-ui/react"
+
+export const Stack = (props: StackProps) => h(_Stack, props)
+export const Heading = (props: HeadingProps) => h(_Heading, props)
