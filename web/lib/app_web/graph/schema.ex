91b8a2d2fcff1cdaad268b8864df019252588d98
Brent Hargrave <brent@brent.is>
2021-09-22 06:35:04 -0500

    . FML: giving up on SDL

--- web/lib/app_web/graph/schema.ex
+++ web/lib/app_web/graph/schema.ex
@@ -4,7 +4,7 @@ defmodule AppWeb.Graph.Schema do
   import_sdl(path: Path.absname("./sdl.gql", __DIR__))
 
   query do
-    field :events, list_of(non_null(:event)) do
+    field :events, non_null(list_of(non_null(:event))) do
       resolve(fn _parent, _args, _context ->
         {:ok, []}
       end)
