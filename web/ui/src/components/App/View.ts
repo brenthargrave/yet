d448e8ce9464091809970250f405fc73842721ff
Brent Hargrave <brent@brent.is>
2021-10-04 10:08:02 -0500

    . WORKS, but man are you wasting time again

--- web/ui/src/components/App/View.ts
+++ web/ui/src/components/App/View.ts
@@ -1,7 +1,17 @@
 import { h } from "@cycle/react"
 import { ChakraProvider } from "@chakra-ui/react"
-import { h1 } from "@cycle/react-dom"
 
-export const View = () => h(ChakraProvider, {}, [])
+import { Landing } from "~/components/Landing"
+import { useRoute, isRoute, routes } from "~/router"
+import { Auth } from "~/components/Auth"
+import { Context } from "~/context"
+
+export const View = ({ context }: { context: Context }) => {
+  const route = useRoute()
+  return h(ChakraProvider, [
+    isRoute(route, routes.home) && h(Landing),
+    isRoute(route, routes.in) && h(Auth, { context }),
+  ])
+}
 
 View.displayName = "AppView"
--- web/ui/src/components/App/index.ts
+++ web/ui/src/components/App/index.ts
@@ -1,30 +1,20 @@
 import React from "react"
 import { h } from "@cycle/react"
 import { ApolloProvider } from "@apollo/client"
-import { ChakraProvider } from "@chakra-ui/react"
 
-import { RouteProvider, useRoute, isRoute, routes } from "~/router"
+import { RouteProvider } from "~/router"
 import { client } from "~/graph"
 import { context } from "~/context"
-import { Landing } from "~/components/Landing"
-import { Auth } from "~/components/Auth"
+import { View } from "./View"
 
 export const App = () => {
-  const route = useRoute()
   return h(React.StrictMode, [
     h(RouteProvider, [
       h(
         ApolloProvider,
         // @ts-ignore
         { client },
-        [
-          h(ChakraProvider, [
-            // currentRoute.name === "home" && h(Landing),
-            isRoute(route, routes.home) && h(Landing),
-            // currentRoute.name === "in" && h(Auth, { context }),
-            isRoute(route, routes.in) && h(Auth, { context }),
-          ]),
-        ]
+        [h(View, { context })]
       ),
     ]),
   ])
