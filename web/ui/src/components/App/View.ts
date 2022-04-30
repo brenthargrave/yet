b2a58f329d1c6f0a91b0663ba98d0635751b9e55
Brent Hargrave <brent@brent.is>
2022-04-30 16:28:26 -0500

    . ...

--- web/ui/src/components/App/View.ts
+++ web/ui/src/components/App/View.ts
@@ -1,17 +1,8 @@
-import React, { FC, Fragment } from "react"
+import React, { FC } from "react"
 import { h } from "@cycle/react"
-import { h1, i } from "@cycle/react-dom"
-import type { Route } from "type-route"
 import { ChakraProvider } from "@chakra-ui/react"
 import { useToast, UseToastOptions } from "@chakra-ui/toast"
 import { ErrorBoundary } from "@sentry/react"
-
-import { routes, isRoute, useRoute } from "~/router"
-
-import { Landing } from "~/components/Landing"
-import { Context, context } from "~/context"
-import { PhoneSubmit } from "~/components/Auth/PhoneSubmit"
-import { PhoneVerify } from "~/components/Auth/PhoneVerify"
 
 /*
 type NotifyConfig = Pick<UseToastOptions, "status" | "title">
@@ -29,25 +20,12 @@ export const View = () => {
       position: "top",
     })
   }
-  const route = useRoute()
-  return h(Fragment, [
-    isRoute(route, routes.home()) && h(Landing),
-    isRoute(route, routes.in()) && h(PhoneSubmit, { context, notify }),
-    isRoute(route, routes.verify()) && h(PhoneVerify, {}),
-  ])
 }
 View.displayName = "AppView"
 */
 
 export const View: FC = ({ children }) => {
   return h(React.StrictMode, [
-    h(ErrorBoundary, { showDialog: true }, [
-      h(ChakraProvider, [
-        children,
-        // h1(`hello, world`),
-        // isRoute(route, routes.home()) && h(Landing),
-        // isRoute(route, routes.in()) && h(Auth, { context }),
-      ]),
-    ]),
+    h(ErrorBoundary, { showDialog: true }, [h(ChakraProvider, [children])]),
   ])
 }
