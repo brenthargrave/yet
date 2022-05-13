91b58304cab6073ff0cd8be51c437a5499adb2ef
Brent Hargrave <brent@brent.is>
2022-05-13 12:56:13 -0500

    ?

--- web/lib/app_web/resolvers/verification.ex
+++ web/lib/app_web/resolvers/verification.ex
@@ -3,5 +3,6 @@ defmodule AppWeb.Resolvers.Verification do
 
   def create(_parent, %{input: %{e164: e164}} = _args, _resolution) do
     Auth.create_verification(e164)
+    {:ok, "PENDING"}
   end
 end
