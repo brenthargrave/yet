6ef85b52504842a73e179ab19593f57ec85a49aa
Brent Hargrave <brent@brent.is>
2022-05-14 16:12:40 -0500

    . FML: twilio rejected my code!?

--- web/lib/app/auth/twilio.ex
+++ web/lib/app/auth/twilio.ex
@@ -32,14 +32,14 @@ defmodule App.Auth.Twilio do
         )
 
       IO.puts(inspect(res))
+      res
     else
       case e164 do
         "+15005550000" ->
           stub_error()
 
         _ ->
-          nil
-          # stub_check()
+          stub_check()
       end
     end
   end
@@ -56,44 +56,86 @@ defmodule App.Auth.Twilio do
 
   @spec stub_create() :: {:ok, map()}
   defp stub_create() do
-    # {
-    #   :ok,
-    #   %ExTwilio.Verify.Verifications{
-    #     account_sid: "AC6965c1da82c9b5bb93d99ec45caeb781",
-    #     amount: nil,
-    #     channel: "sms",
-    #     date_created: "2022-05-13T20:24:50Z",
-    #     date_updated: "2022-05-13T20:25:36Z",
-    #     lookup: %{
-    #       carrier: %{
-    #         error_code: nil,
-    #         mobile_country_code: "310",
-    #         mobile_network_code: "800",
-    #         name: "T-Mobile USA, Inc.",
-    #         type: "mobile"
-    #       }
-    #     },
-    #     payee: nil,
-    #     send_code_attempts: [
-    #       %{
-    #         attempt_sid: "VL3ecb5a2f13be6639cc79eac2d6af87d2",
-    #         channel: "sms",
-    #         time: "2022-05-13T20:24:50.000Z"
-    #       },
-    #       %{
-    #         attempt_sid: "VLebcda58186753204136aee6421c68238",
-    #         channel: "sms",
-    #         time: "2022-05-13T20:25:36.654Z"
-    #       }
-    #     ],
-    #     service_sid: "VAfc95ba97399b05f59d304d30b4d5961f",
-    #     sid: "VE0fa64bb52d598df5afe780542b5107d9",
-    #     status: "pending",
-    #     to: "+19099103449",
-    #     url:
-    #       "https://verify.twilio.com/v2/Services/VAfc95ba97399b05f59d304d30b4d5961f/Verifications/VE0fa64bb52d598df5afe780542b5107d9",
-    #     valid: false
-    #   }
-    # }
+    {
+      :ok,
+      %ExTwilio.Verify.Verifications{
+        account_sid: "AC6965c1da82c9b5bb93d99ec45caeb781",
+        amount: nil,
+        channel: "sms",
+        date_created: "2022-05-13T20:24:50Z",
+        date_updated: "2022-05-13T20:25:36Z",
+        lookup: %{
+          carrier: %{
+            error_code: nil,
+            mobile_country_code: "310",
+            mobile_network_code: "800",
+            name: "T-Mobile USA, Inc.",
+            type: "mobile"
+          }
+        },
+        payee: nil,
+        send_code_attempts: [
+          %{
+            attempt_sid: "VL3ecb5a2f13be6639cc79eac2d6af87d2",
+            channel: "sms",
+            time: "2022-05-13T20:24:50.000Z"
+          },
+          %{
+            attempt_sid: "VLebcda58186753204136aee6421c68238",
+            channel: "sms",
+            time: "2022-05-13T20:25:36.654Z"
+          }
+        ],
+        service_sid: "VAfc95ba97399b05f59d304d30b4d5961f",
+        sid: "VE0fa64bb52d598df5afe780542b5107d9",
+        status: "pending",
+        to: "+19099103449",
+        url:
+          "https://verify.twilio.com/v2/Services/VAfc95ba97399b05f59d304d30b4d5961f/Verifications/VE0fa64bb52d598df5afe780542b5107d9",
+        valid: false
+      }
+    }
+  end
+
+  defp stub_check() do
+    {
+      :ok,
+      %ExTwilio.Verify.Verifications{
+        account_sid: "AC6965c1da82c9b5bb93d99ec45caeb781",
+        amount: nil,
+        channel: "sms",
+        date_created: "2022-05-14T21:07:20Z",
+        date_updated: "2022-05-14T21:07:37Z",
+        lookup: %{
+          carrier: %{
+            error_code: nil,
+            mobile_country_code: "310",
+            mobile_network_code: "800",
+            name: "T-Mobile USA, Inc.",
+            type: "mobile"
+          }
+        },
+        payee: nil,
+        send_code_attempts: [
+          %{
+            attempt_sid: "VLd60e0e8bbc0959fa8a60d487bf420637",
+            channel: "sms",
+            time: "2022-05-14T21:07:20.000Z"
+          },
+          %{
+            attempt_sid: "VLaadbb353293d7d5218c093ed5019d6cd",
+            channel: "sms",
+            time: "2022-05-14T21:07:37.078Z"
+          }
+        ],
+        service_sid: "VAfc95ba97399b05f59d304d30b4d5961f",
+        sid: "VEeeff22efa3ecbfe53e836b234e8a42e3",
+        status: "pending",
+        to: "+19099103449",
+        url:
+          "https://verify.twilio.com/v2/Services/VAfc95ba97399b05f59d304d30b4d5961f/Verifications/VEeeff22efa3ecbfe53e836b234e8a42e3",
+        valid: false
+      }
+    }
   end
 end
