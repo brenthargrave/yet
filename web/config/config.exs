048a3b53fe59c24bdc15bdb73ada5c5da4f27378
Brent Hargrave <brent@brent.is>
2022-05-19 10:37:49 -0500

    default UTC w/ microseconds support

--- web/config/config.exs
+++ web/config/config.exs
@@ -22,7 +22,8 @@ config :app, App.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-  migration_primary_key: [name: :id, type: :binary_id]
+  migration_primary_key: [name: :id, type: :binary_id],
+  migration_timestamps: [type: :utc_datetime_usec]
 
 config :phoenix, :template_engines,
   slim: PhoenixSlime.Engine,
--- web/lib/app/model.ex
+++ web/lib/app/model.ex
@@ -4,8 +4,9 @@ defmodule App.Model do
   defmacro __using__(_) do
     quote do
       use Ecto.Schema
-      @primary_key {:id, Ecto.ULID, autogenerate: true}
+      @primary_key {:id, Ecto.ULID, autogenerate: false}
       @foreign_key_type Ecto.ULID
+      @timestamps_opts [type: :utc_datetime_usec]
     end
   end
 end
