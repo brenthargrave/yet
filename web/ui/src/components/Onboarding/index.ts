1dbe47dc315b652d8039e1bc5e75edeb69302195
Brent Hargrave <brent@brent.is>
2022-05-23 10:22:50 -0500

    . WIP

--- web/ui/src/components/Onboarding/index.ts
+++ web/ui/src/components/Onboarding/index.ts
@@ -1,13 +1,17 @@
 import { h, ReactSource } from "@cycle/react"
-import { of } from "rxjs"
+import { map, of } from "rxjs"
 import { View } from "./View"
 import { t } from "~/i18n"
+import { Source as GraphSource } from "~/graph"
 
 interface Sources {
   react: ReactSource
+  graph: GraphSource
 }
 
 export const Onboarding = (sources: Sources) => {
+  const { me$ } = sources.graph
+  const state$ = me$.pipe(map)
   /*
   TODO: onboarding flow:
   - What's your full name? (name)
