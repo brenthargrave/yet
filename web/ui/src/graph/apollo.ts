e64cc607ec6d769c16b5afb948b8a6f90b5e4565
Brent Hargrave <brent@brent.is>
2022-05-25 11:05:19 -0500

    . persistent cache - really need await?

--- web/ui/src/graph/apollo.ts
+++ web/ui/src/graph/apollo.ts
@@ -1,10 +1,6 @@
-import {
-  ApolloClient,
-  InMemoryCache,
-  createHttpLink,
-  NormalizedCacheObject,
-} from "@apollo/client"
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client"
 import { setContext } from "@apollo/client/link/context"
+import { persistCache, LocalStorageWrapper } from "apollo3-cache-persist"
 
 const httpLink = createHttpLink({
   uri: "/graphql",
@@ -23,10 +19,16 @@ const authLink = setContext((_, { headers }) => {
   }
 })
 
-export type Client = ApolloClient<NormalizedCacheObject>
+const cache = new InMemoryCache()
+
+// TODO: https://github.com/apollographql/apollo-cache-persist/tree/eeb3efe83a6e0bee18a3e396f9abf1dc57077e4b#web
+// > await before instantiating ApolloClient, else queries might run before the cache is persisted
+await persistCache({
+  cache,
+  storage: new LocalStorageWrapper(window.localStorage),
+})
 
 export const client = new ApolloClient({
   link: authLink.concat(httpLink),
-  // TODO: persistent cache
-  cache: new InMemoryCache(),
+  cache,
 })
