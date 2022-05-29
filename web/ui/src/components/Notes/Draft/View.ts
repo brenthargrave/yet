9542f72c15670d4307deec78164472251e4c2ba0
Brent Hargrave <brent@brent.is>
2022-05-29 16:18:00 -0500

    . left addon?

--- web/ui/src/components/Notes/Draft/View.ts
+++ web/ui/src/components/Notes/Draft/View.ts
@@ -1,6 +1,7 @@
+import { InputLeftAddon } from "@chakra-ui/react"
 import { h } from "@cycle/react"
 import { CreatableSelect, ChakraStylesConfig } from "chakra-react-select"
-import { Heading, Stack, Center } from "~/system"
+import { Heading, Stack, Center, InputGroup } from "~/system"
 
 export interface Option {
   value: string
@@ -28,24 +29,27 @@ export const View = ({ options, onSelect }: Props) =>
     [
       h(Heading, { size: "lg" }, `Note a new conversation`),
       h(Stack, { direction: "row", alignItems: "center", width: "100%" }, [
-        h(Heading, { size }, "with:"),
-        h(CreatableSelect, {
-          placeholder: "Dale Carnegie",
-          autoFocus: true,
-          size,
-          chakraStyles: {
-            container: (provided, state) => ({
-              ...provided,
-              width: "100%",
-            }),
-          },
-          isClearable: true,
-          createOptionPosition: "first",
-          formatCreateLabel: (inputValue) => `New contact: "${inputValue}"`,
-          // @ts-ignore
-          onChange: (newValue, _meta) => onSelect(newValue),
-          options,
-        }),
+        // h(Heading, { size }, "with:"),
+        h(InputGroup, [
+          h(InputLeftAddon, { children: "With:" }),
+          h(CreatableSelect, {
+            placeholder: "Dale Carnegie",
+            autoFocus: true,
+            size,
+            chakraStyles: {
+              container: (provided, state) => ({
+                ...provided,
+                width: "100%",
+              }),
+            },
+            isClearable: true,
+            createOptionPosition: "first",
+            formatCreateLabel: (inputValue) => `Add contact: "${inputValue}"`,
+            // @ts-ignore
+            onChange: (newValue, _meta) => onSelect(newValue),
+            options,
+          }),
+        ]),
       ]),
       // TODO: optional, editable fields for when / where
       h(Stack, { direction: "row", alignItems: "center", width: "100%" }, [
