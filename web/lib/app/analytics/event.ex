836e762a2c7d35b34a5002617ddd8f289cb3b3eb
Brent Hargrave <brent@brent.is>
2022-06-10 14:48:41 -0500

    . flesh out events table

--- web/lib/app/analytics/event.ex
+++ web/lib/app/analytics/event.ex
@@ -5,13 +5,15 @@ defmodule App.Analytics.Event do
   typed_schema "events" do
     field :anon_id, :string
     field :name, :string
+    belongs_to :customer, App.Customer
+    field :properties, :map
     timestamps(type: :utc_datetime_usec)
   end
 
   @doc false
   def changeset(event, attrs) do
     event
-    |> cast(attrs, [:name, :anon_id])
+    |> cast(attrs, [:name, :anon_id, :customer_id])
     |> validate_required([:name, :anon_id])
   end
 end
--- web/priv/repo/migrations/20211019204145_create_events.exs
+++ web/priv/repo/migrations/20211019204145_create_events.exs
@@ -2,16 +2,6 @@ defmodule App.Repo.Migrations.CreateEvents do
   use Ecto.Migration
 
   def change do
-    create table(:events) do
-      add :anon_id, :string, null: false
-      add :name, :string, null: false
-      add :customer_id, :string
-      timestamps()
-    end
-
-    create(index(:events, [:anon_id]))
-    create(index(:events, [:customer_id]))
-
     create table(:customers) do
       add :e164, :string, null: false
       add :token, :string, null: false
@@ -24,9 +14,19 @@ defmodule App.Repo.Migrations.CreateEvents do
 
     create(unique_index(:customers, [:e164]))
     create(index(:customers, [:token]))
-    # profile
     create(index(:customers, [:name]))
     create(index(:customers, [:org]))
     create(index(:customers, [:role]))
+
+    create table(:events) do
+      add :anon_id, :string, null: false
+      add :name, :string, null: false
+      add :properties, :map
+      add :customer_id, references("customers")
+      timestamps()
+    end
+
+    create(index(:events, [:anon_id]))
+    create(index(:events, [:customer_id]))
   end
 end
