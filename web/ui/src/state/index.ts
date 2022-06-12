6a07aaa41e3aad8933021a4ba53f0b599bed577f
Brent Hargrave <brent@brent.is>
2022-06-12 10:52:27 -0500

    . xstate

---
+++ web/ui/src/state/index.ts
@@ -0,0 +1,62 @@
+import { interpret, createMachine } from "xstate"
+import { from } from "rxjs"
+
+interface ToggleContext {
+  count: number
+}
+
+type ToggleEvent = {
+  type: "TOGGLE"
+}
+
+const machine = createMachine<ToggleContext, ToggleEvent>({
+  id: "root",
+  initial: "inactive",
+  context: {
+    count: 0,
+  },
+  states: {
+    inactive: {
+      on: { TOGGLE: "active" },
+    },
+    active: {
+      on: { TOGGLE: "inactive" },
+    },
+  },
+})
+
+const interpreter = interpret(machine)
+const service = interpreter.start()
+
+export const state$ = from(service)
+
+service.send("TOGGLE")
+service.send("TOGGLE")
+
+// type Sink = Stream<Command>
+
+// export function makeDriver(): Driver<Sink, Source> {
+//   return function (sink: Sink): Source {
+//     sink.addListener({
+//       next: ({ type, route }) => {
+//         match(type)
+//           .with(CommandType.push, () => route?.push())
+//           .exhaustive()
+//       },
+//       error: (error) => console.error(error),
+//       complete: () => console.info("complete"),
+//     })
+
+//     const history$ = new Observable<Route>((observer) => {
+//       observer.next(session.getInitialRoute())
+//       const unlisten = session.listen((route) => {
+//         observer.next(route)
+//       })
+//       return unlisten
+//     }).pipe(tag("history$"), shareReplay({ bufferSize: 1, refCount: true }))
+
+//     return {
+//       history$,
+//     }
+//   }
+// }
