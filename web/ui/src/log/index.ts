427d771a520e4300c13b25e67655ccfde1a5dfe4
Brent Hargrave <brent@brent.is>
2022-08-24 16:25:57 -0500

    skip js logging in prod

--- web/ui/src/log/index.ts
+++ web/ui/src/log/index.ts
@@ -2,8 +2,11 @@ import { create } from "rxjs-spy" // no filter, logs everything
 
 import { tag } from "rxjs-spy/operators/tag"
 
+// @ts-ignore
+const { VITE_API_ENV: env } = import.meta.env
+
 const spy = create({ defaultLogger: console, sourceMaps: true })
-spy.log()
+if (env !== "prod") spy.log()
 
 const makeTagger =
   <T>(scope: string) =>
