9a771a8ebcd80fb55183358fd3e2b080a5212871
Brent Hargrave <brent@brent.is>
2022-08-25 12:27:35 -0500

    . revert Opp#show

--- web/ui/src/components/App/Home/Opps/Single/Show/View.ts
+++ web/ui/src/components/App/Home/Opps/Single/Show/View.ts
@@ -1,10 +1,19 @@
-import { Heading, Spacer, Stack } from "@chakra-ui/react"
+import {
+  Divider,
+  Heading,
+  Spacer,
+  Stack,
+  List,
+  ListItem,
+} from "@chakra-ui/react"
 import { h } from "@cycle/react"
 import { FC } from "react"
 import { OppView } from "~/components/Opp"
 import { Customer, Maybe, Opp } from "~/graph"
 import { FullWidthVStack, Header, Nav } from "~/system"
 import { Location } from ".."
+import { isNotLastItem } from "~/fp"
+import { ConversationView } from "~/components/Conversation"
 
 export interface Props {
   viewer: Maybe<Customer>
@@ -13,15 +22,53 @@ export interface Props {
   onClickBack?: () => void
 }
 
-export const View: FC<Props> = ({ viewer, opp, location, onClickBack }) =>
-  h(FullWidthVStack, {}, [
+export const View: FC<Props> = ({ viewer, opp, location, onClickBack }) => {
+  const { conversations } = opp
+  return h(FullWidthVStack, {}, [
     h(Nav, { onClickBack }),
     h(Header, {}, [
       //
       h(Heading, { size: "md" }, "Opportunity"),
       h(Spacer),
     ]),
-    h(OppView, { opp, viewer }),
+    h(
+      Stack,
+      {
+        direction: "column",
+        alignItems: "start",
+        width: "100%",
+        // spacing: 1,
+        gap: 4,
+      },
+      [
+        h(OppView, { opp, viewer }),
+        // mention history
+        h(Divider, { orientation: "horizontal" }),
+        h(FullWidthVStack, { gap: 4, pt: 0 }, [
+          h(Header, {}, [h(Heading, { size: "sm" }, "Mentions")]),
+          h(List, { width: "100%", spacing: 8, pl: 2, pr: 2, pt: 0 }, [
+            ...conversations.map((conversation, idx, all) => {
+              return h(
+                ListItem,
+                {
+                  /* padding? */
+                },
+                [
+                  h(ConversationView, {
+                    viewer,
+                    conversation,
+                    maxLines: 10,
+                    showStatus: false,
+                  }),
+                  isNotLastItem(idx, all) && h(Divider, { padding: 4 }),
+                ]
+              )
+            }),
+          ]),
+        ]),
+      ]
+    ),
   ])
+}
 
 View.displayName = "Opps.Show"
--- web/ui/src/components/Opp/index.ts
+++ web/ui/src/components/Opp/index.ts
@@ -1,25 +1,18 @@
 import {
-  Stack,
   HStack,
+  Icon,
   Spacer,
   Tag,
-  VStack,
-  Icon,
   TagLabel,
   Text,
-  Divider,
-  Heading,
-  List,
-  ListItem,
+  VStack,
 } from "@chakra-ui/react"
 import { h } from "@cycle/react"
 import { FC } from "react"
 import { TbArrowsSplit2 } from "react-icons/tb"
 import { Customer, Maybe, Opp } from "~/graph"
-import { MarkdownView, bold, i, Header, FullWidthVStack } from "~/system"
 import { formatMoney } from "~/i18n"
-import { ConversationView } from "../Conversation"
-import { isNotLastItem } from "~/fp"
+import { bold, FullWidthVStack, i, MarkdownView } from "~/system"
 
 export interface Props {
   viewer: Maybe<Customer>
@@ -27,76 +20,40 @@ export interface Props {
 }
 
 export const OppView: FC<Props> = ({ viewer, opp }) => {
-  const { role, org, desc, fee, conversations } = opp
-  return h(
-    Stack,
-    {
-      direction: "column",
-      alignItems: "start",
-      width: "100%",
-      // spacing: 1,
-      gap: 4,
-    },
-    [
-      h(FullWidthVStack, {}, [
-        h(HStack, { width: "100%", alignItems: "start" }, [
+  const { role, org, desc, fee } = opp
+  return h(FullWidthVStack, {}, [
+    h(HStack, { width: "100%", alignItems: "start" }, [
+      h(
+        VStack,
+        {
+          alignItems: "start",
+          gap: 0,
+          spacing: 1,
+        },
+        [
+          h(MarkdownView, { md: `${bold(role)}` }),
+          h(MarkdownView, { md: `${i(org)}` }),
+        ]
+      ),
+      h(Spacer),
+      // Fee section
+      fee.amount > 0 &&
+        h(VStack, { alignItems: "center", padding: 2 }, [
           h(
-            VStack,
+            Tag,
             {
-              alignItems: "start",
-              gap: 0,
-              spacing: 1,
+              variant: "outline",
+              colorScheme: "green",
             },
             [
-              h(MarkdownView, { md: `${bold(role)}` }),
-              h(MarkdownView, { md: `${i(org)}` }),
+              h(Icon, { as: TbArrowsSplit2 }),
+              h(TagLabel, { pl: 2 }, formatMoney(fee)),
             ]
           ),
-          h(Spacer),
-          // Fee section
-          fee.amount > 0 &&
-            h(VStack, { alignItems: "center", padding: 2 }, [
-              h(
-                Tag,
-                {
-                  variant: "outline",
-                  colorScheme: "green",
-                },
-                [
-                  h(Icon, { as: TbArrowsSplit2 }),
-                  h(TagLabel, { pl: 2 }, formatMoney(fee)),
-                ]
-              ),
-            ]),
         ]),
-        desc && h(Text, { size: "md", noOfLines: 1 }, desc),
-      ]),
-      // mention history
-      h(Divider, { orientation: "horizontal" }),
-      h(FullWidthVStack, { gap: 4, pt: 0 }, [
-        h(Header, {}, [h(Heading, { size: "sm" }, "Mentions")]),
-        h(List, { width: "100%", spacing: 8, pl: 2, pr: 2, pt: 0 }, [
-          ...conversations.map((conversation, idx, all) => {
-            return h(
-              ListItem,
-              {
-                /* padding? */
-              },
-              [
-                h(ConversationView, {
-                  viewer,
-                  conversation,
-                  maxLines: 10,
-                  showStatus: false,
-                }),
-                isNotLastItem(idx, all) && h(Divider, { padding: 4 }),
-              ]
-            )
-          }),
-        ]),
-      ]),
-    ]
-  )
+    ]),
+    desc && h(Text, { size: "md", noOfLines: 1 }, desc),
+  ])
 }
 
 OppView.displayName = "OppView"
