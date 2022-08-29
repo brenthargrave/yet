66888d094d0af8e9e2b70d618f45feb4a094e5e6
Brent Hargrave <brent@brent.is>
2022-08-29 10:48:16 -0500

    fix: just refresh on load

--- web/ui/src/components/App/Home/Timeline/index.ts
+++ web/ui/src/components/App/Home/Timeline/index.ts
@@ -2,6 +2,7 @@ import { h, ReactSource } from "@cycle/react"
 import {
   combineLatest,
   distinctUntilChanged,
+  EMPTY,
   filter,
   map,
   merge,
@@ -12,19 +13,15 @@ import {
 import { isNotNullish } from "rxjs-etc"
 import { filterResultOk } from "ts-results/rxjs-operators"
 import { Source as ActionSource } from "~/action"
-import {
-  Conversation,
-  getTimeline$,
-  Source as GraphSource,
-  subscribeTimeline$,
-} from "~/graph"
+import { Conversation, getTimeline$, Source as GraphSource } from "~/graph"
 import { makeTagger } from "~/log"
-import { NEWID, push, routes } from "~/router"
+import { NEWID, push, routes, Source as RouterSource } from "~/router"
 import { cb$, shareLatest } from "~/rx"
 import { State, View } from "./View"
 
 export interface Sources {
   react: ReactSource
+  router: RouterSource
   graph: GraphSource
   action: ActionSource
 }
@@ -34,8 +31,10 @@ export const Timeline = (sources: Sources, tagPrefix?: string) => {
   const tag = makeTagger(tagScope)
 
   const {
-    graph: { me$ },
+    router: { history$ },
+    graph: { me$: _me$ },
   } = sources
+  const me$ = _me$.pipe(filter(isNotNullish), tag("me$"))
 
   const [onClickNew, clickNew$] = cb$(tag("clickNew$"))
   const newConvo$ = clickNew$.pipe(
@@ -47,33 +46,22 @@ export const Timeline = (sources: Sources, tagPrefix?: string) => {
     map(({ id }) => push(routes.conversation({ id })))
   )
 
-  const result$ = getTimeline$().pipe(tag("result$"), share())
-
-  const initiaLevents$ = result$.pipe(
-    filterResultOk(),
-    startWith([]),
-    tag("THIS events$"),
-    share()
-  )
-
-  const newEvents$ = me$.pipe(
-    filter(isNotNullish),
-    switchMap(({ id }) => subscribeTimeline$({ id })),
-    tag("newEvents$"),
+  const result$ = history$.pipe(
+    switchMap((route) =>
+      route.name === routes.root.name ? getTimeline$() : EMPTY
+    ),
+    tag("result$"),
     shareLatest()
   )
 
-  const events$ = merge(
-    initiaLevents$,
-    newEvents$.pipe(
-      switchMap(() => getTimeline$()),
-      filterResultOk(),
-      tag("newEvents$ -> refetched"),
-      shareLatest()
-    )
+  const events$ = result$.pipe(
+    filterResultOk(),
+    startWith([]),
+    tag("events$"),
+    share()
   )
 
-  const state$ = initiaLevents$.pipe(
+  const state$ = events$.pipe(
     map(() => State.ready),
     startWith(State.loading),
     distinctUntilChanged(),
