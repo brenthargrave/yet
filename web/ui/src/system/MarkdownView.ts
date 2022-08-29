3019c6b14a54127c2dae4dccf3a8515e8c152cc3
Brent Hargrave <brent@brent.is>
2022-08-29 15:50:40 -0500

    fix: local md links forced page reload

--- web/ui/src/system/MarkdownView.ts
+++ web/ui/src/system/MarkdownView.ts
@@ -6,6 +6,8 @@ import remarkBreaks from "remark-breaks"
 import remarkGfm from "remark-gfm"
 import { style } from "typestyle"
 import { join, split, take, trim } from "~/fp"
+import { session } from "~/router"
+import { Box } from "."
 
 export const bold = (value: string) => `**${value}**`
 export const i = (value: string) => `*${value}*`
@@ -13,7 +15,7 @@ export const i = (value: string) => `*${value}*`
 const className = style({ textDecoration: "underline" })
 
 // @ts-ignore
-const { VITE_HOST } = import.meta.env
+const { VITE_HOST: HOST } = import.meta.env
 
 interface Props {
   md?: string | null
@@ -34,17 +36,32 @@ export const MarkdownView: FC<Props> = ({ md, maxLines }) => {
   // TODO: PR fix on remarkBreaks lib
   // https://github.com/remarkjs/react-markdown/issues/278#issuecomment-628264062
   children = children.replace(/\n/gi, "\n &nbsp;")
-  return h(ReactMarkdown, {
-    children,
-    remarkPlugins: [remarkBreaks, remarkGfm],
-    // NOTE: open off-domain links in new tab
-    // linkTarget: "_blank"
-    linkTarget: (href, children, title) =>
-      href.includes(VITE_HOST) ? "_self" : "_blank",
-    skipHtml: false,
-    components: {
-      a: ({ node, href, children, ...props }) =>
-        a({ href, className, ...props }, children),
-    },
-  })
+
+  // NOTE: prevent local links from reloading page
+  const onClick = (event: React.MouseEvent<HTMLElement>) => {
+    // @ts-ignore
+    const { href } = event.target
+    if (href && href.includes(HOST)) {
+      event.preventDefault()
+      const url = new URL(href)
+      const { pathname } = url
+      session.push(pathname)
+    }
+  }
+
+  return h(Box, { onClick }, [
+    h(ReactMarkdown, {
+      children,
+      remarkPlugins: [remarkBreaks, remarkGfm],
+      // NOTE: open off-domain links in new tab
+      // linkTarget: "_blank"
+      linkTarget: (href, children, title) =>
+        href.includes(HOST) ? "_self" : "_blank",
+      skipHtml: false,
+      components: {
+        a: ({ node, href, children, ...props }) =>
+          a({ href, className, ...props }, children),
+      },
+    }),
+  ])
 }
