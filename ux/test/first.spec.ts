7e8e5b03fb635cc33ce1d862b9c531c7a8775183
Brent Hargrave <brent@brent.is>
2022-09-03 11:59:28 -0500

    . mocha + assert works w/ ts just fine

---
+++ ux/test/first.spec.ts
@@ -0,0 +1,8 @@
+import assert from "assert";
+import { describe } from "mocha";
+
+describe("TEST", () => {
+  it("is true", () => {
+    assert(true)
+  })
+})
--- ux/test/index.ts
+++ ux/test/index.ts
@@ -1,4 +1,15 @@
 import { Observable } from "rxjs";
+import Mocha from "mocha"
+
+const mocha = new Mocha()
+
+mocha.addFile(`./test/first.spec.ts`);
+
+mocha.run((failures) => {
+  process.on('exit', () => {
+    process.exit(failures)
+  });
+});
 
 console.log("Hello, world!")
 
