5c22e83c027a188d5693c08970d156fb6b051724
Brent Hargrave <brent@brent.is>
2022-09-03 12:43:22 -0500

    looks like parallelization is possible, drop ava

--- ux/index.ts
+++ ux/index.ts
@@ -1,6 +1,8 @@
 import { Observable } from "rxjs";
 import Mocha from "mocha"
 
+export * from "./src/browser"
+
 const mocha = new Mocha()
 
 mocha.addFile(`./test/first.spec.ts`);
@@ -13,6 +15,6 @@ const run = mocha.run((failures) => {
 
 // TODO: make sure to clear files on reruns
 // mocha.unloadFiles()
-
-console.log("Hello, world!")
+// TODO: parallelize tests, call before run
+// mocha.parallelMode(true)
 
---
+++ ux/src/browser.ts
@@ -0,0 +1,47 @@
+import * as puppeteer from "puppeteer"
+import { startsWith } from "lodash"
+import { log } from "./log"
+
+export const makeBrowser = async () => {
+  // TODO: share across invocations
+  const browser = await puppeteer.launch({
+    dumpio: true, //TODO: DEBUG only
+  })
+  // NOTE: prefer incognito contexts for isolation across test runs
+  // https://git.io/fjLt1
+  const context = await browser.createIncognitoBrowserContext()
+  const page = await context.newPage()
+  page.setDefaultTimeout(1 * 1000)
+
+  const visit = async (path: string): Promise<void> => {
+    const { HOST, PORT_SSL } = process.env
+    const host = `https://${HOST}:${PORT_SSL}`
+    const url = startsWith(path, "http") ? path : `${host}${path}`
+    log.debug(url)
+    await page.goto(url)
+  }
+
+  const close = async () => {
+    await page.close()
+    await context.close()
+    await browser.close()
+  }
+
+  // TODO: extract page txt, substring match
+  const see = async (ariaLabelValue: string) => {
+    const sel = `[aria-label="${ariaLabelValue}"]`
+    await page.waitForSelector(sel, { visible: true })
+  }
+
+  const tap = async (ariaLabelValue: string) => {
+    const sel = `[aria-label="${ariaLabelValue}"]`
+    await page.tap(sel)
+  }
+
+  return {
+    close,
+    visit,
+    see,
+    tap,
+  }
+}
