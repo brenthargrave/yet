44ce61670f4f5655f3533964b46485b7b14be777
Brent Hargrave <brent@brent.is>
2022-09-03 19:12:38 -0500

    . better, with click/tap workaround (painful)

--- ux/tests/example.spec.ts
+++ ux/tests/example.spec.ts
@@ -1,33 +1,19 @@
 import { test, expect } from '@playwright/test';
 
-// test('homepage has Playwright in title and get started link linking to the intro page', async ({ page, }) => {
-//   await page.goto('https://playwright.dev/');
+test('Sign up', async ({ page, browser, context }) => {
+  await page.goto("/")
+  const signUp = page.locator('button', { hasText: "Create Account"});
+  await signUp.focus()
+  await page.keyboard.press("Enter")
+  await expect(page.locator("text=phone number")).toBeVisible()
+  await page.locator("#phone-number").fill("9099103449")
+  await page.locator("button", { hasText: "Continue"}).focus()
+  await page.keyboard.press("Enter")
+  await page.screenshot({fullPage: true, path: './scratch/a.png'})
 
-//   // Expect a title "to contain" a substring.
-//   await expect(page).toHaveTitle(/Playwright/);
-
-//   // create a locator
-//   const getStarted = page.locator('text=Get Started');
-
-//   // Expect an attribute "to be strictly equal" to the value.
-//   await expect(getStarted).toHaveAttribute('href', '/docs/intro');
-
-//   // Click the get started link.
-//   await getStarted.click();
-
-//   // Expects the URL to contain intro.
-//   await expect(page).toHaveURL(/.*intro/);
-// });
-
-test('Sign up', async ({ page, browser }) => {
-  // const a = await browser.newPage()
-  // a.goto("https://localhost:6443/")
-  // a.goto("/in")
-  page.goto("/")
-  const signUp = page.locator('text=Create Account');
-  await signUp.click()
-  const screenie = await  page.screenshot({fullPage: true, path: 'screenshot.png'})
-  // page.locator("text=phone")
-  // expect(a.locator("text=phone number")).toHaveText()
-  // expect(a.
+  const b = await browser.newPage()
+  // const b = await bc.newPage()
+  await b.goto("/")
+  await expect(b.locator("text=TBD")).toBeVisible()
+  await b.screenshot({fullPage: true, path: './scratch/b.png'})
 })
