e490a55540b26c8541b2a88d83962ea6b5bf0bb5
Brent Hargrave <brent@brent.is>
2022-09-10 13:28:31 -0700

    . test outline

--- web/ux/test/first.spec.ts
+++ web/ux/test/first.spec.ts
@@ -18,13 +18,31 @@ it("Sign up", async () => {
   await exit()
 })
 
-it("Create conversation", async () => {
+it("Opp reward payment", async () => {
   const { customer, exit } = await makeBrowser()
   const a = await customer(Alice)
   try {
     await a.signup()
     await a.click("Conversations")
-    // await a.
+    // TODO
+    // Alice create Opp with award
+    // Alice creates conversation w/ Bob, mentoining Opp
+    // Bob cosigns
+    // ? verify timeline updates?
+    //
+    // Bob creates converastion w/ Charlie, mentions Opp
+    // Charlie cosigns
+    // ? timeline updates?
+
+    // Charlie note w/ David, mentions Opp
+    // David cosigns
+    // (Alice hires David...)
+    // Alice clicks Opp
+    // Alice clicks Pay Reward
+    // Alice sees modal w/ David, Charlie, Bob in list, nested list of their convs
+    // Alice clicks Charlie > pay
+    // Alice (pays with Stripe?)
+    // ? timeline updates?
   } catch (error) {
     console.error(error)
     a.screenie()
