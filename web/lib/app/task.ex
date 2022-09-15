d9a2b4f69a862e559285beb2260aa236832a618d
Brent Hargrave <brent@brent.is>
2022-09-15 11:26:36 -0500

    WIP: explicit db checkout in test seems to work

--- web/lib/app/task.ex
+++ web/lib/app/task.ex
@@ -1,11 +1,24 @@
 use Croma
 
+alias App.{
+  Repo
+}
+
 defmodule App.Task do
   @type callback() :: (() -> any())
 
   defun async_nolink(callback :: callback()) :: any() do
-    Task.Supervisor.async_nolink(App.TaskSupervisor, fn ->
-      callback.()
-    end)
+    if System.get_env("MIX_ENV") == "test" do
+      parent = self()
+
+      Task.Supervisor.async_nolink(App.TaskSupervisor, fn ->
+        Ecto.Adapters.SQL.Sandbox.allow(Repo, parent, self())
+        callback.()
+      end)
+    else
+      Task.Supervisor.async_nolink(App.TaskSupervisor, fn ->
+        callback.()
+      end)
+    end
   end
 end
